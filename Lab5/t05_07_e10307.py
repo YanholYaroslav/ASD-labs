99f54e77ecbfd3dc656a1f146bfa7c4fcef9e45e
YanholYaroslav <116906888+YanholYaroslav@users.noreply.github.com>
2023-03-10 01:50:15 +0200

    lab5 done + lab 6 started

--- Lab5/t05_07_e10307.py
+++ Lab5/t05_07_e10307.py
@@ -5,27 +5,28 @@ def is_poss_place(amount, places, x):
         if i - prev_place >= x:
             c += 1
             prev_place = i
+    
     return c >= amount
 
-
 def bin_search(amount, places):
-    left, right = 0, places[-1] - places[0] + 1
-    while right - left > 1:
-        mid = (left + right) // 2
-        if is_poss_place(amount, places, mid):
-            left = mid
-        else:
-            right = mid
-    return left
+	l = 0
+	r = places[-1] - places[0] + 1
+	while r - l > 1:
+		m = l + (r - l) // 2
+		if is_poss_place(amount, places, m):
+			l = m
+		else: r = m
+		
+	return l
 
 
 if __name__ == "__main__":
-    n, m = map(int, input().split())
-    places = []
+    n, m = list(map(int, input().split()))
+    places = list()
     for i in range(m):
-        a, b = map(int, input().split())
-        places.append(a)
-        places.append(b)
-    places.sort()
-    res = bin_search(n, places)
-    print(res)
+        a, b = list(map(int, input().split()))
+        for j in range (a, b + 1):
+            places.append(j)
+    
+    d = bin_search(n, places)
+    print(d)
